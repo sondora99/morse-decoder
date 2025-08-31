9bcc00cccd21b911391de8af8a54e1c059a2a205
sondora99 <marymikser@gmail.com>
2025-08-31 19:47:55 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,30 +36,29 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-function decode(expr) {
-    let words = expr.split('**********');
-  
-    return words.map(word => {
+module.exports = function decode(expr) {
+  const words = expr.split('**********');
+
+  return words
+    .map((word) => {
       // Делим каждую часть на символы по 10 символов
-      let letters = [];
+      const letters = [];
       for (let i = 0; i < word.length; i += 10) {
         letters.push(word.slice(i, i + 10));
       }
-      
-      // Декодируем каждый символ Морзе в букву
-      return letters.map(letter => {
-        let morse = '';
-        for (let i = 0; i < letter.length; i += 2) {
-          if (letter[i] === '1' && letter[i + 1] === '0') morse += '.';
-          if (letter[i] === '1' && letter[i + 1] === '1') morse += '-';
-        }
-        return MORSE_TABLE[morse];
-      }).join('');
-    }).join(' ');
-}
 
-module.exports = {
-    decode
-}
+      // Декодируем каждый символ Морзе в букву
+      return letters
+        .map((letter) => {
+          let morse = '';
+          for (let i = 0; i < letter.length; i += 2) {
+            if (letter[i] === '1' && letter[i + 1] === '0') morse += '.';
+            if (letter[i] === '1' && letter[i + 1] === '1') morse += '-';
+          }
+          return MORSE_TABLE[morse];
+        })
+        .join('');
+    })
+    .join(' ');
+};
